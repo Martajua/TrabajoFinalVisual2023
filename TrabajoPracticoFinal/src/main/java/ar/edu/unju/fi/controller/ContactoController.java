758fd7d0fc1e92a9e5ba9db835585b9054a47eae
Ariel Saravia <diciembre93@gmail.com>
2023-07-01 17:35:57 -0300

    primer avance clase controladora del formulario de contacto

---
+++ TrabajoPracticoFinal/src/main/java/ar/edu/unju/fi/controller/ContactoController.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class ContactoController {
+	/**
+	 * Metodo que captura una peticion http en la forma de una url
+	 * devuelve la pagina a mostrar.
+	 * @return
+	 */
+	@GetMapping("/contacto")
+	public String getIndex() {
+		return "contacto";
+	}
+}
