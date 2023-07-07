c35f1cc1f711119f8870679bf6c680660edd17b6
Ariel Saravia <diciembre93@gmail.com>
2023-07-07 15:22:34 -0300

    se agregaron las notaciones necesarias para crear las tablas.

--- TrabajoPracticoFinal/src/main/java/ar/edu/unju/fi/entity/Ingrediente.java
+++ TrabajoPracticoFinal/src/main/java/ar/edu/unju/fi/entity/Ingrediente.java
@@ -1,72 +1,77 @@
 package ar.edu.unju.fi.entity;
 
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-
+@Component
+@Entity
+@Table(name = "ingredientes")
 public class Ingrediente {
 
-    //region Attributes
-    private Integer id;
-    private Boolean estado;
-    @NotBlank(
-            message = "Introduce un nombre"
-    )
-    @Size(
-            min = 3,
-            max = 30,
-            message = "El nombre solo puede contener entre 3 y 30 caracteres"
-    )
-    @Pattern(
-            regexp = "[a-z A-Z]+",
-            message = "Solo puede contener letras"
-    )
-    private String nombre;
-    //endregion
+	// region Attributes
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ingre_id")
+	private Integer id;
+	
+	@Column(name = "ingre_estado")
+	private Boolean estado;
+	
+	@Column(name = "ingre_nombre")
+	@NotBlank(message = "Introduce un nombre")
+	@Size(min = 3, max = 30, message = "El nombre solo puede contener entre 3 y 30 caracteres")
+	@Pattern(regexp = "[a-z A-Z]+", message = "Solo puede contener letras")
+	private String nombre;
+	// endregion
 
-    //region Constructors
-    public Ingrediente() {
-    }
+	// region Constructors
+	public Ingrediente() {
+	}
 
-    public Ingrediente(
-            Integer id,
-            Boolean estado,
-            String nombre) {
+	public Ingrediente(Integer id, Boolean estado, String nombre) {
 
-        this.id = id;
-        this.estado = estado;
-        this.nombre = nombre;
+		this.id = id;
+		this.estado = estado;
+		this.nombre = nombre;
 
-    }
-    //endregion
+	}
+	// endregion
 
-    //region Getters and Setters
-    public Integer getId() {
-        return id;
-    }
+	// region Getters and Setters
+	public Integer getId() {
+		return id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public Boolean getEstado() {
-        return estado;
-    }
+	public Boolean getEstado() {
+		return estado;
+	}
 
-    public void setEstado(Boolean estado) {
-        this.estado = estado;
-    }
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    //endregion
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	// endregion
 
-    //region Methods
-    //endregion
+	// region Methods
+	// endregion
 
 }
--- TrabajoPracticoFinal/src/main/java/ar/edu/unju/fi/entity/Usuario.java
+++ TrabajoPracticoFinal/src/main/java/ar/edu/unju/fi/entity/Usuario.java
@@ -1,5 +1,11 @@
 package ar.edu.unju.fi.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.*;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
@@ -7,178 +13,154 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 
 @Component
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
 
-    //region Attributes
-    private Integer id;
-    private Boolean estado;
-    @NotBlank(
-            message = "Introduce un nombre"
-    )
-    @Size(
-            min = 3,
-            max = 30,
-            message = "El nombre solo puede contener entre 3 y 30 caracteres"
-    )
-    @Pattern(
-            regexp = "[a-z A-Z]+",
-            message = "Solo puede contener letras"
-    )
-    private String nombre;
-    @NotBlank(
-            message = "Introduce un apellido"
-    )
-    @Size(
-            min = 3,
-            max = 30,
-            message = "El apellido solo puede contener entre 3 y 30 caracteres"
-    )
-    @Pattern(
-            regexp = "[a-z A-Z]+",
-            message = "Solo puede contener letras"
-    )
-    private String apellido;
+	// region Attributes
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="usu_id")
+	private Integer id;
+	
+	@Column(name = "usu_estado")
+	private Boolean estado;
+	
+	@Column(name = "usu_nombre")
+	@NotBlank(message = "Introduce un nombre")
+	@Size(min = 3, max = 30, message = "El nombre solo puede contener entre 3 y 30 caracteres")
+	@Pattern(regexp = "[a-z A-Z]+", message = "Solo puede contener letras")
+	private String nombre;
+	
+	@Column(name = "usu_apellido")
+	@NotBlank(message = "Introduce un apellido")
+	@Size(min = 3, max = 30, message = "El apellido solo puede contener entre 3 y 30 caracteres")
+	@Pattern(regexp = "[a-z A-Z]+", message = "Solo puede contener letras")
+	private String apellido;
+	
+	@Column(name = "usu_email")
+	@Email(message = "Email no es correcto", regexp = ".*")
+	@NotBlank(message = "Debes introducir un email")
+	private String email;
+	
+	@Column(name = "usu_fechaNacimiento")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@PastOrPresent(message = "La fecha ingresada es incorrecta")
+	@NotNull(message = "Debes introducir una fecha")
+	private LocalDate fechaNacimiento;
+	
+	@Column(name = "usu_telefono")
+	@NotBlank(message = "Debes introducir un número de teléfono")
+	@Pattern(regexp = "0388-[0-9]{3}-[0-9]{4}")
+	private String telefono;
+	
+	@Column(name = "usu_sexo")
+	@NotBlank(message = "Debes introducir tu sexo")
+	@Pattern(regexp = "masculino|femenino", flags = Pattern.Flag.CASE_INSENSITIVE)
+	private String sexo;
+	
+	@Column(name = "usu_estatura")
+	@NotEmpty(message = "Debes introducir tu estatura")
+	@Min(0)
+	private Float estatura;
+	// endregion
 
-    @Email(
-            message = "Email no es correcto", regexp = ".*"
-    )
-    @NotBlank(
-            message = "Debes introducir un email"
-    )
-    private String email;
-    @DateTimeFormat(
-            pattern = "yyyy-MM-dd"
-    )
-    @PastOrPresent(
-            message = "La fecha ingresada es incorrecta"
-    )
-    @NotNull(
-            message = "Debes introducir una fecha"
-    )
-    private LocalDate fechaNacimiento;
-    @NotBlank(
-            message = "Debes introducir un número de teléfono"
-    )
-    @Pattern(
-            regexp = "0388-[0-9]{3}-[0-9]{4}"
-    )
-    private String telefono;
-    @NotBlank(
-            message = "Debes introducir tu sexo"
-    )
-    @Pattern(
-            regexp = "masculino|femenino",
-            flags = Pattern.Flag.CASE_INSENSITIVE
-    )
-    private String sexo;
-    @NotEmpty(message = "Debes introducir tu estatura")
-    @Min(0)
-    private Float estatura;
-    //endregion
+	// region Constructors
+	public Usuario() {
+	}
 
-    //region Constructors
-    public Usuario() {
-    }
+	public Usuario(Integer id, Boolean estado, String nombre, String apellido, String email, LocalDate fechaNacimiento,
+			String telefono, String sexo, Float estatura) {
 
-    public Usuario(
-            Integer id,
-            Boolean estado,
-            String nombre,
-            String apellido,
-            String email,
-            LocalDate fechaNacimiento,
-            String telefono,
-            String sexo,
-            Float estatura) {
+		this.id = id;
+		this.estado = estado;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.fechaNacimiento = fechaNacimiento;
+		this.telefono = telefono;
+		this.sexo = sexo;
+		this.estatura = estatura;
 
-        this.id = id;
-        this.estado = estado;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.email = email;
-        this.fechaNacimiento = fechaNacimiento;
-        this.telefono = telefono;
-        this.sexo = sexo;
-        this.estatura = estatura;
+	}
+	// endregion
 
-    }
-    //endregion
+	// region Getters and Setters
+	public Integer getId() {
+		return id;
+	}
 
-    //region Getters and Setters
-    public Integer getId() {
-        return id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public Boolean getEstado() {
+		return estado;
+	}
 
-    public Boolean getEstado() {
-        return estado;
-    }
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
 
-    public void setEstado(Boolean estado) {
-        this.estado = estado;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public String getApellido() {
+		return apellido;
+	}
 
-    public String getApellido() {
-        return apellido;
-    }
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
 
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
 
-    public LocalDate getFechaNacimiento() {
-        return fechaNacimiento;
-    }
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
 
-    public void setFechaNacimiento(LocalDate fechaNacimiento) {
-        this.fechaNacimiento = fechaNacimiento;
-    }
+	public String getTelefono() {
+		return telefono;
+	}
 
-    public String getTelefono() {
-        return telefono;
-    }
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
 
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
+	public String getSexo() {
+		return sexo;
+	}
 
-    public String getSexo() {
-        return sexo;
-    }
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
 
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
+	public Float getEstatura() {
+		return estatura;
+	}
 
-    public Float getEstatura() {
-        return estatura;
-    }
+	public void setEstatura(Float estatura) {
+		this.estatura = estatura;
+	}
+	// endregion
 
-    public void setEstatura(Float estatura) {
-        this.estatura = estatura;
-    }
-    //endregion
-
-    //region Methods
-    //endregion
+	// region Methods
+	// endregion
 
 }
