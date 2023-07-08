7379cab620a8d95b1b8ac8e2dbef28e8fd0ca8f9
Ariel Saravia <diciembre93@gmail.com>
2023-07-08 15:32:16 -0300

    se dejo funcional la vista de pagina de inicio.

---
+++ TrabajoPracticoFinal/src/main/java/ar/edu/unju/fi/controller/IngredienteController.java
@@ -0,0 +1,20 @@
+/*
+ * package ar.edu.unju.fi.controller;
+ * 
+ * import org.springframework.stereotype.Controller; import
+ * org.springframework.ui.Model; import
+ * org.springframework.web.bind.annotation.GetMapping; import
+ * org.springframework.web.bind.annotation.RequestMapping;
+ * 
+ * import ar.edu.unju.fi.service.IIngredienteService;
+ * 
+ * @Controller
+ * 
+ * @RequestMapping("/ingrediente") public class IngredienteController {
+ * 
+ * private IIngredienteService ingredienteService;
+ * 
+ * @GetMapping("/listado") public String getListaRecetas(Model model) {
+ * model.addAttribute("ingredientes",ingredienteService.getAllIngredientes());
+ * return "ingredientes"; } }
+ */
